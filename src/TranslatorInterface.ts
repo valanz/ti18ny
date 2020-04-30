d8176416e3e17f10f30fdfc495c9460eb53f4ae5
Valentin Menardie <valentin.menardie@gmail.com>
2020-04-30 10:00:52 +0200

    Add TranslatorInterface

---
+++ src/TranslatorInterface.ts
@@ -0,0 +1,25 @@
+export default interface TranslatorInterface {
+    /**
+     * Register some translation keys for a given locale
+     * @param {string} locale - the targeted locale
+     * @param {object} keys - the keys to be registered in the dictionnary
+     */
+    register(locale:string, keys:Object);
+
+    /**
+     * Translate a given key into a localized string
+     * @param {string} key - the key (represented in the following format: "some.key")
+     * @param {array} parameters - the parameters that should be set in the final localized string
+     * @return {string} the localized string
+     */
+    translate(key:string, parameters:string[]):string;
+
+    /**
+     * Replace placeholders by the given parameters
+     * @param {string} string - the string to work on
+     * @param {array} parameters - the parameters
+     * @return {string} the string with the proper parameters
+     */
+    replaceWithArguments(string:string, parameters:string[]):string;
+}
+
